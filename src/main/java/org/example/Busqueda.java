0fecfca8bd5b4928a21d37696569945de813fcd9
JuanRios <juand.rios@autonoma.edu.co>
2026-06-12 15:35:27 -0500

    se agrego el metodo bubble sort

--- src/main/java/org/example/Busqueda.java
+++ src/main/java/org/example/Busqueda.java
@@ -14,4 +14,35 @@ public class Busqueda {
        }
        return -1;
     }
+
+    public class BubbleSort {
+        public static void bubbleSort(int[] arr) {
+            int n = arr.length;
+            boolean intercambiado;
+
+            // Bucle externo para recorrer todo el array
+            for (int i = 0; i < n - 1; i++) {
+                intercambiado = false;
+
+                // Bucle interno para comparar elementos adyacentes
+                for (int j = 0; j < n - 1 - i; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        // Intercambio si el elemento actual es mayor que el siguiente
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        intercambiado = true;
+                    }
+                }
+
+                // Optimización: si no hubo intercambios, el array ya está ordenado
+                if (!intercambiado) {
+                    break;
+                }
+            }
+        }
+
+    }
+
 }
