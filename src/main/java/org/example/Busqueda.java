e4304a014487ef2ca453a67db4c6a9826bb06fac
JuanRios <juand.rios@autonoma.edu.co>
2026-06-12 15:44:19 -0500

    metodo merge sort nuevo

--- src/main/java/org/example/Busqueda.java
+++ src/main/java/org/example/Busqueda.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Arrays;
 
 public class Busqueda {
     public int buscar(int[] lista, int valor){
@@ -45,4 +46,75 @@ public class Busqueda {
 
     }
 
-}
+
+
+
+        // Método principal para probar el algoritmo
+        public static void main(String[] args) {
+            int[] arreglo = {38, 27, 43, 3, 9, 82, 10};
+
+            System.out.println("Arreglo original: " + Arrays.toString(arreglo));
+
+            mergeSort(arreglo);
+
+            System.out.println("Arreglo ordenado: " + Arrays.toString(arreglo));
+        }
+
+        // Método que divide el arreglo recursivamente
+        public static void mergeSort(int[] arreglo) {
+            int n = arreglo.length;
+
+            // Caso base: si el arreglo tiene 1 o 0 elementos, ya está ordenado
+            if (n < 2) {
+                return;
+            }
+
+            int mitad = n / 2;
+            int[] izquierdo = new int[mitad];
+            int[] derecho = new int[n - mitad];
+
+            // Llenar el arreglo izquierdo
+            for (int i = 0; i < mitad; i++) {
+                izquierdo[i] = arreglo[i];
+            }
+
+            // Llenar el arreglo derecho
+            for (int i = mitad; i < n; i++) {
+                derecho[i - mitad] = arreglo[i];
+            }
+
+            // Llamadas recursivas para dividir ambas mitades
+            mergeSort(izquierdo);
+            mergeSort(derecho);
+
+            // Fusionar las mitades ordenadas
+            fusionar(arreglo, izquierdo, derecho);
+        }
+
+        // Método que fusiona los arreglos ordenados
+        public static void fusionar(int[] arreglo, int[] izquierdo, int[] derecho) {
+            int i = 0, j = 0, k = 0;
+
+            // Comparar elementos de ambos arreglos y ordenarlos en el arreglo original
+            while (i < izquierdo.length && j < derecho.length) {
+                if (izquierdo[i] <= derecho[j]) {
+                    arreglo[k++] = izquierdo[i++];
+                } else {
+                    arreglo[k++] = derecho[j++];
+                }
+            }
+
+            // Copiar los elementos restantes del arreglo izquierdo (si los hay)
+            while (i < izquierdo.length) {
+                arreglo[k++] = izquierdo[i++];
+            }
+
+            // Copiar los elementos restantes del arreglo derecho (si los hay)
+            while (j < derecho.length) {
+                arreglo[k++] = derecho[j++];
+            }
+        }
+    }
+
+
+
